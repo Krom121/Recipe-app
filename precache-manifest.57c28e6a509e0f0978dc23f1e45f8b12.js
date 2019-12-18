ece9b07abe30e8b5599cfc8bb2e2383a9708366d
stephen <stephenmacgregor9@gmail.com>
2019-12-18 18:47:22 +0000

    Updates

---
+++ precache-manifest.57c28e6a509e0f0978dc23f1e45f8b12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e312e600e10de91d8169e91ace743af4",
+    "url": "/Recipe-app/index.html"
+  },
+  {
+    "revision": "ce05e1b70a38e40edf2f",
+    "url": "/Recipe-app/static/css/main.3ab7d3b0.chunk.css"
+  },
+  {
+    "revision": "aafea4ee91827378808d",
+    "url": "/Recipe-app/static/js/2.dda751c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Recipe-app/static/js/2.dda751c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce05e1b70a38e40edf2f",
+    "url": "/Recipe-app/static/js/main.76c6d232.chunk.js"
+  },
+  {
+    "revision": "d09518616294797cd0a5",
+    "url": "/Recipe-app/static/js/runtime-main.bad6ab18.js"
+  }
+]);
